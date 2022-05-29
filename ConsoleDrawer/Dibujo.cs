38e1840b97728d7221ce316a263a68303097e1f8
Antonio Léon <txonik@hotmail.com>
2022-05-29 12:56:49 +0200

    Nueva clase Lista y Cambios en Dibujo

--- ConsoleDrawer/Dibujo.cs
+++ ConsoleDrawer/Dibujo.cs
@@ -2,19 +2,25 @@ namespace ConsoleDrawer
 {
     public class Dibujo
     {
-        private Figura[] aFiguras;
+        private Lista aFiguras;
 
         public Dibujo()
         {
-            aFiguras = new Figura[10];
-            aFiguras[0] = new Texto(10, 5, "Hola a Todes");
+            aFiguras = new Lista(10);
+            aFiguras.Add(new Texto(10, 5, "Hola a Todes")) ;
         }
 
         public void Dibujar()
         {
-            foreach(var aFigura in aFiguras)
+            /*foreach(var aFigura in aFiguras)
             {
                 aFigura?.Dibujar();
+            }*/
+
+            for(var idx = 0; idx < aFiguras.Count; idx++)
+            {
+                var figura = aFiguras.GetAt(idx) as Figura;
+                figura.Dibujar();                
             }
         }
     }
