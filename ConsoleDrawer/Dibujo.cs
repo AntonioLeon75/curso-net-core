d21393a3bb3520ba462cfa8a785c604794d32f58
Antonio Léon <txonik@hotmail.com>
2022-06-20 21:04:58 +0200

    Varianza de genéricos

--- ConsoleDrawer/Dibujo.cs
+++ ConsoleDrawer/Dibujo.cs
@@ -2,18 +2,33 @@ namespace ConsoleDrawer
 {
     public class Dibujo
     {
-        private Lista<IFigura> aFiguras;
+        private Lista<IFigura> _figuras;
 
         public Dibujo()
         {
-            aFiguras = new Lista<IFigura>(10);
-            aFiguras.Add(new Texto(10, 5, "Hola a Todes"));
-            aFiguras.Add(new Cuadrado(2,10));
+            _figuras = new Lista<IFigura>(10);
+            _figuras.Add(new Texto(10, 5, "Hola a Todes"));
+            _figuras.Add(new Cuadrado(2,10));
+
+            var lista2 = new Lista<Texto>(10);
+            //Foo2(lista2);
+
+            Foo(lista2);
+        }
+
+        public void Foo2(Lista<IFigura> figuras)
+        {
+
+        }
+
+        public void Foo(IEnumerable<IFigura> figuras)
+        {
+            foreach(var f in figuras){}
         }
 
         public void Dibujar()
         {
-            foreach(var aFigura in aFiguras)
+            foreach(var aFigura in _figuras)
             {
                 aFigura?.Dibujar();
             }
