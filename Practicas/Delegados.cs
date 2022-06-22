139584a51dd21bcd5c1a6ecb0e8f8536eae13b40
Antonio Léon <txonik@hotmail.com>
2022-06-22 20:30:54 +0200

    práctica 2 de delegados

--- Practicas/Delegados.cs
+++ Practicas/Delegados.cs
@@ -15,9 +15,48 @@ namespace Practicas
             handler("Soy un delegado básico");
         }
 
+
+        delegate void Operacion(int a, int b);
+        void Suma(int a, int b)
+        {
+            Console.WriteLine("La suma de " + a.ToString() + " y " + b.ToString() + " es " + (a + b).ToString());
+        }
+
+        void Multiplica(int a, int b)
+        {
+            Console.WriteLine("La multiplicación de " + a.ToString() + " por " + b.ToString() + " es " + (a * b).ToString());
+        }
+
+        void ExecOperaciones()
+        {
+            Operacion opDel;
+
+            Console.WriteLine("Ejemplo de Delegados con operaciones");
+
+            for (int i = 0; i <= 30; i++)
+            {
+                int rA = new Random().Next(1, 100);
+                int rB = new Random().Next(1, 100);
+
+                if (i % 3 == 0)
+                {
+                    opDel = Suma;
+                    opDel(rA, rB);
+                }
+                else if (i % 5 == 0)
+                {
+                    opDel = Multiplica;
+                    opDel(rA, rB);
+                }
+
+                Thread.Sleep(500);
+            }
+        }
+
         public void ExecMainDelegados()
         {
             ExecBasicPractice();
+            ExecOperaciones();
         }
     }
 }
